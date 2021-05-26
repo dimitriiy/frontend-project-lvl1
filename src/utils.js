d0c3cfca4adb02947a266e50f5380c37c2fbbad8
Mazitov Dmitry <mazitov.dima@mail.ru>
2021-05-26 22:51:56 +0300

    Fixed eslint line len

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import { ANSWER_NO, ANSWER_YES } from './constants.js';
 
+// eslint-disable-next-line max-len
 export const getRandomIntNumber = (min = 0, max = 1000) => Math.floor(Math.random() * (max - min) + min);
 export const getRandomIntPair = () => Array.from({ length: 2 }).map(() => getRandomIntNumber());
 export const isValidAnswer = (str) => [ANSWER_YES, ANSWER_NO].includes(str);
